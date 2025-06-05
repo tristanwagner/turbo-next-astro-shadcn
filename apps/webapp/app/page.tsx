b9e3b44ec13db5c8d84b58d664c51112a63822cf
Tristan WAGNER <t.wagner66250@gmail.com>
2025-06-06 01:52:42 +0200

    feat(ui): add shadcn

---
+++ apps/webapp/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/components/button"
+import { ModeToggle } from "@repo/ui/components/ModeToggle";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <Button>Hello there</Button>
+      <ModeToggle />
+    </main>
+  );
+}
