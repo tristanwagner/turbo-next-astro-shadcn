da89fc612e33f937e43a822823b87fa139ca8486
Tristan WAGNER <t.wagner66250@gmail.com>
2025-06-06 15:25:28 +0200

    feat(prettier): add prettier astro + run format

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@repo/ui/components/button"
+import { Button } from "@repo/ui/components/button";
 import { ModeToggle } from "@repo/ui/components/ModeToggle";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Shadcn button in Nextjs!</Button>
       <ModeToggle />
     </main>
